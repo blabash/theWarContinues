f941984b2196d5d18435651c548e851d1b49fea0
brett <brett.labash@gmail.com>
2021-02-16 20:13:41 -0800

    Complete React Setup

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//Component
+//State
+//Lifecycle
+//UI
+
+class App extends Component {
+  render() {
+    return <div>Hello Fap!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;

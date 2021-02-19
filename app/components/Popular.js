7658917f94a944e879b81e57bc73e83a72c4ca0c
brett <brett.labash@gmail.com>
2021-02-19 00:52:33 -0800

    Add color change functionality on selected navbar button

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 export default class Popular extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All',
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+    });
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
@@ -8,7 +24,15 @@ export default class Popular extends Component {
       <ul className='flex-center'>
         {languages.map((lang) => (
           <li key={lang}>
-            <button className='btn-clear nav-link'>{lang}</button>
+            <button
+              onClick={() => this.updateLanguage(lang)}
+              className='btn-clear nav-link'
+              style={
+                lang === this.state.selectedLanguage ? { color: `blue` } : null
+              }
+            >
+              {lang}
+            </button>
           </li>
         ))}
       </ul>

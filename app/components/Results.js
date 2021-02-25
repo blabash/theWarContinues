999b3fa513a198e0a49c9065558855293b91bbfa
brett <brett.labash@gmail.com>
2021-02-25 00:52:19 -0800

    Add Profile List component

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { battle } from '../utils/api';
 import {
   FaUser,
@@ -9,6 +10,45 @@ import {
   FaCode,
 } from 'react-icons/fa';
 import Card from './Card';
+
+function ProfileList({ profile }) {
+  return (
+    <ul className='card-list'>
+      <li>
+        <FaUser color='rgb(239, 115, 115)' size={22} />
+        {profile.name}
+      </li>
+      {profile.location && (
+        <li>
+          <FaCompass color='rgb(144, 115, 255)' size={22} />
+          {profile.location}
+        </li>
+      )}
+      {profile.company && (
+        <li>
+          <FaBriefcase color='#795548' size={22} />
+          {profile.company}
+        </li>
+      )}
+      <li>
+        <FaUsers color='rgb(129, 195, 245)' size={22} />
+        {profile.followers.toLocaleString()} followers
+      </li>
+      <li>
+        <FaUserFriends color='rgb(64, 195, 95)' size={22} />
+        {profile.following.toLocaleString()} following
+      </li>
+      <li>
+        <FaCode color='black' size={22} />
+        {profile.public_repos.toLocaleString()} repos
+      </li>
+    </ul>
+  );
+}
+
+ProfileList.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
 
 export default class Results extends Component {
   constructor(props) {
@@ -60,36 +100,7 @@ export default class Results extends Component {
           href={winner.profile.html_url}
           name={winner.profile.login}
         >
-          <ul className='card-list'>
-            <li>
-              <FaUser color='rgb(239, 115, 115)' size={22} />
-              {winner.profile.name}
-            </li>
-            {winner.profile.location && (
-              <li>
-                <FaCompass color='rgb(144, 115, 255)' size={22} />
-                {winner.profile.location}
-              </li>
-            )}
-            {winner.profile.company && (
-              <li>
-                <FaBriefcase color='#795548' size={22} />
-                {winner.profile.company}
-              </li>
-            )}
-            <li>
-              <FaUsers color='rgb(129, 195, 245)' size={22} />
-              {winner.profile.followers.toLocaleString()} followers
-            </li>
-            <li>
-              <FaUserFriends color='rgb(64, 195, 95)' size={22} />
-              {winner.profile.following.toLocaleString()} following
-            </li>
-            <li>
-              <FaCode color='black' size={22} />
-              {winner.profile.public_repos.toLocaleString()} repos
-            </li>
-          </ul>
+          <ProfileList profile={winner} />
         </Card>
         <Card
           header={winner.score === loser.score ? 'Tie' : 'Loser'}
@@ -98,36 +109,7 @@ export default class Results extends Component {
           href={loser.profile.html_url}
           name={loser.profile.login}
         >
-          <ul className='card-list'>
-            <li>
-              <FaUser color='rgb(239, 115, 115)' size={22} />
-              {loser.profile.name}
-            </li>
-            {loser.profile.location && (
-              <li>
-                <FaCompass color='rgb(144, 115, 255)' size={22} />
-                {loser.profile.location}
-              </li>
-            )}
-            {loser.profile.company && (
-              <li>
-                <FaBriefcase color='#795548' size={22} />
-                {loser.profile.company}
-              </li>
-            )}
-            <li>
-              <FaUsers color='rgb(129, 195, 245)' size={22} />
-              {loser.profile.followers.toLocaleString()} followers
-            </li>
-            <li>
-              <FaUserFriends color='rgb(64, 195, 95)' size={22} />
-              {loser.profile.following.toLocaleString()} following
-            </li>
-            <li>
-              <FaCode color='black' size={22} />
-              {loser.profile.public_repos.toLocaleString()} repos
-            </li>
-          </ul>
+          <ProfileList profile={loser} />
         </Card>
       </div>
     );
